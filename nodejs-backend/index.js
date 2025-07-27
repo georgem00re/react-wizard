58ee3acdfec6987ddd696e69ceefa97e8a7aa6cf
george <george.moore@cyted.ai>
2025-07-27 18:48:42 +0100

    use ejs to generate an example html file and add a download endpoint to the nodejs backend

--- nodejs-backend/index.js
+++ nodejs-backend/index.js
@@ -1,9 +1,32 @@
 
 const express = require("express");
 const app = express();
+const path = require("path");
+const ejs = require("ejs");
 
 app.get("/health", (req, res) => {
     res.status(200).json({ status: "ok" });
 })
 
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.get("/download", async (req, res) => {
+    const filePath = path.join(__dirname, "templates", "example.html.ejs");
+
+    try {
+        const htmlContent = await ejs.renderFile(filePath, {});
+
+        res.set({
+            "Content-Type": "text/html",
+            "Content-Disposition": `attachment; filename="example.html"`,
+            "Content-Length": Buffer.byteLength(htmlContent),
+        });
+
+        res.send(htmlContent);
+    } catch (err) {
+        console.log(err)
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Listening on http://localhost:3000')
+});
