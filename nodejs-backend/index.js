71cbe9044194167131a977b96720998d6a137ad6
george <george.moore@cyted.ai>
2025-07-28 18:13:37 +0100

    update the download endpoint to return a zip archive file

--- nodejs-backend/index.js
+++ nodejs-backend/index.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require("path");
 const ejs = require("ejs");
 const cors = require("cors");
+const archiver = require("archiver");
 
 app.use(cors());
 
@@ -12,18 +13,19 @@ app.get("/health", (req, res) => {
 })
 
 app.get("/download", async (req, res) => {
-    const filePath = path.join(__dirname, "templates", "example.html.ejs");
+    const indexHtmlTemplatePath = path.join(__dirname, "templates", "index.html.ejs");
 
     try {
-        const htmlContent = await ejs.renderFile(filePath, {});
+        res.setHeader("Content-Type", "application/zip")
+        res.setHeader("Content-Disposition", 'attachment; filename="rendered_files.zip"');
 
-        res.set({
-            "Content-Type": "text/html",
-            "Content-Disposition": `attachment; filename="example.html"`,
-            "Content-Length": Buffer.byteLength(htmlContent),
-        });
+        const archive = archiver("zip", { zlib: { level: 9 } });
+        archive.pipe(res);
 
-        res.send(htmlContent);
+        const html = await ejs.renderFile(indexHtmlTemplatePath, {})
+        archive.append(html, { name: "index.html" })
+
+        await archive.finalize()
     } catch (err) {
         console.log(err)
         res.status(500).send("Internal Server Error");
