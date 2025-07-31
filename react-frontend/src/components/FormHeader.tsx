671ae40d53f5026d1db0df226baf93f4b8d88115
george <george.moore@cyted.ai>
2025-07-31 18:15:29 +0100

    add a new logo and icon to the form header

--- react-frontend/src/components/FormHeader.tsx
+++ react-frontend/src/components/FormHeader.tsx
@@ -1,12 +1,14 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHatWizard} from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import {GMLogo} from "./GMLogo.tsx";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faWandSparkles} from "@fortawesome/free-solid-svg-icons";
 
 export const FormHeader = () => {
     return (
-        <div className="flex items-center space-x-2 border-b py-3 border-gray-200">
-            <FontAwesomeIcon icon={faHatWizard} size="2x"/>
-            <h1 className="text-xl text-gray-700">George's React Wizard</h1>
+        <div className="flex items-center space-x-2 border-b py-4 border-gray-200">
+            <GMLogo/>
+            <FontAwesomeIcon icon={faWandSparkles} className="text-blue-600 text-2xl"/>
+            <h1 className="text-xl text-gray-700 ml-2">George's React Wizard</h1>
         </div>
     )
 }
